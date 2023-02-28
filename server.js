12e9b3df38413cd7cb5e961e729e0619d0536602
Hurduz Oksana <hurduzoksana@gmail.com>
2023-02-28 15:42:38 +0200

    refactored with mongoDB

--- server.js
+++ server.js
@@ -1,5 +1,31 @@
-const app = require('./app')
+const app = require("./app");
+const mongoose = require("mongoose");
+const { URL, PORT = 3000 } = process.env;
+mongoose.set("strictQuery", true);
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
+// mongoose
+//   .connect(URL, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() =>
+//     app.listen(PORT, () => {
+//       console.log("Database connection successful");
+//     })
+//   )
+//   .catch((error) => {
+//     console.log(error.message);
+//     process.exit(1);
+//   });
+
+mongoose
+  .connect(URL)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log("Database connection successful");
+    });
+  })
+  .catch((error) => {
+    console.log(error.message);
+    process.exit(1);
+  });
