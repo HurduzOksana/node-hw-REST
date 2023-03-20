79a94804ed5cc6e6bec80b0230fb1f43f4995af8
Hurduz Oksana <hurduzoksana@gmail.com>
2023-03-20 11:29:04 +0200

    added logic of avatars addition

--- controllers/auth/registration.js
+++ controllers/auth/registration.js
@@ -1,6 +1,7 @@
 const bcrypt = require("bcrypt");
 const { User } = require("../../models/user");
 const { RequestError } = require("../../helpers");
+const gravatar = require("gravatar");
 
 const registration = async (req, res) => {
   const { email, password } = req.body;
@@ -11,8 +12,13 @@ const registration = async (req, res) => {
   }
 
   const hashPassword = await bcrypt.hash(password, 10);
+  const avatarURL = gravatar.url(email);
 
-  const result = await User.create({ ...req.body, password: hashPassword });
+  const result = await User.create({
+    ...req.body,
+    password: hashPassword,
+    avatarURL,
+  });
 
   res.status(201).json({
     name: result.name,
